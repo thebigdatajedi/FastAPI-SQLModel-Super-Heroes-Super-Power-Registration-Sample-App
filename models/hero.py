0418cf062cd8875ee55b8aae13d1c1dee1b650b9
Gabe Cruz <gabe.cruz@henryscheinone.com>
2023-04-14 18:17:17 -0600

    added comment explaining how the one to many relationship connects via the python/pydantic/sqlalchemy syntax

--- models/hero.py
+++ models/hero.py
@@ -8,4 +8,7 @@ class Hero(SQLModel, table=True):
     name: str = Field(index=True)
     secret_name: str
     age: Optional[int] = Field(default=None, index=True)
-    team_id: Optional[int] = Field(default=None, foreign_key="team.id")
+    team_id: Optional[int] = Field(default=None, foreign_key="team.id")  # Using SQLModel, in most of the cases
+    # you only need a field (column) with a foreign_key in the Field() with a string pointing to another table
+    # and column to connect two tables.
+    # NOTE::...with a string pointing to another table and column to connect the two tables!!!
